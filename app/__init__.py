22db5602046989f8a1058ec67ed92ef05ad6f862
Connor Mackay <connor.mackay@gmail.com>
2022-10-04 14:44:46 -0400

    setup flask configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,8 @@
 from flask import Flask
+from config import Config
+
 app = Flask(__name__)
+app.config.from_object(Config)
 
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
+from app import routes
 
-if __name__ == '__main__':
-  app.run()
