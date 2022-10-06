76d680fdb03eea9aa99a8a49a87946a74a53bfc7
Connor Mackay <connor.mackay@gmail.com>
2022-10-06 11:31:29 -0400

    implement login form

--- app/routes.py
+++ app/routes.py
@@ -2,16 +2,14 @@ from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
+@app.route('/index')
+def hello_world():
+  return render_template('base.html')
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
   form = LoginForm()
   if form.validate_on_submit:
-    flash('Login requested for user {}, remember_ne={}'.format(
-      form.username.data, form.remember_me.data
-    ))
-    return redirect('/')
+    flash('Login requested for user {}, remember_me={}'.format(
+      form.username.data, form.remember_me.data))
   return render_template('login.html', title='Sign in', form=form)
-
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
