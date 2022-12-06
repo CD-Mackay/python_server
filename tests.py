97b80387a5a8915075b91b22f5021cbb17b05129
Connor Mackay <connor.mackay@gmail.com>
2022-12-06 16:40:20 -0500

    implement follow unit test

--- tests.py
+++ tests.py
@@ -16,3 +16,28 @@ class UserModelCase(unittest.TestCase):
     db.session.remove()
     db.drop_all()
     self.app_context.pop()
+  
+  def test_follow(self):
+    u1 = User(username="John", email="john@example.com")
+    u2 = User(username="Jeff", email="jeff@example.com")
+    db.session.add(u1)
+    db.session.add(u2)
+    db.session.commit()
+    self.assertEqual(u1.followed.all(), [])
+    self.assertEqual(u2.followed.all(), [])
+
+    u1.follow(u2)
+    db.session.commit()
+    self.assertTrue(u1.is_following(u2))
+    self.assertEqual(u1.followed.count(), 1)
+    self.assertEqual(u1.followed.first().username, 'Jeff')
+    self.assertEqual(u2.followers.count(), 1)
+    self.assertEqual(u2.followers.first().username, 'john')
+
+    u1.unfollow(u2)
+    db.session.commit()
+    self.assertFalse(u1.is_following(u2))
+    self.assertEqual(u1.followed.count(), 0)
+    self.assertEqual(u2.followers.count(), 0)
+
+
