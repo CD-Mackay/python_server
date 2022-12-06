b8dc7ae99361c9ac5ba61bccaa503027b900a01f
Connor Mackay <connor.mackay@gmail.com>
2022-12-06 14:29:56 -0500

    add setup and teardown methods to unit tests

--- tests.py
+++ tests.py
@@ -1,2 +1,18 @@
 import os 
 os.environ['DATABASE_URL'] = 'sqlite://'
+
+from datetime import datetime, timedelta
+import unittest
+from app import app, db
+from app.models import User, Post
+
+class UserModelCase(unittest.TestCase):
+  def setUp(self):
+    self.app_context = app.app_context()
+    self.app_context.push()
+    db.create_all()
+  
+  def tearDown(self):
+    db.session.remove()
+    db.drop_all()
+    self.app_context.pop()
