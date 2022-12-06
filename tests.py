e1080de7c00a4f05ce4064ceba5182f2431f98b3
Connor Mackay <connor.mackay@gmail.com>
2022-12-06 16:52:25 -0500

    update unit tests

--- tests.py
+++ tests.py
@@ -6,6 +6,7 @@ import unittest
 from app import app, db
 from app.models import User, Post
 
+
 class UserModelCase(unittest.TestCase):
   def setUp(self):
     self.app_context = app.app_context()
@@ -18,19 +19,19 @@ class UserModelCase(unittest.TestCase):
     self.app_context.pop()
   
   def test_follow(self):
-    u1 = User(username="John", email="john@example.com")
-    u2 = User(username="Jeff", email="jeff@example.com")
+    u1 = User(username='john', email='john@example.com')
+    u2 = User(username='susan', email='susan@example.com')
     db.session.add(u1)
     db.session.add(u2)
     db.session.commit()
     self.assertEqual(u1.followed.all(), [])
-    self.assertEqual(u2.followed.all(), [])
+    self.assertEqual(u1.followers.all(), [])
 
     u1.follow(u2)
     db.session.commit()
     self.assertTrue(u1.is_following(u2))
     self.assertEqual(u1.followed.count(), 1)
-    self.assertEqual(u1.followed.first().username, 'Jeff')
+    self.assertEqual(u1.followed.first().username, 'susan')
     self.assertEqual(u2.followers.count(), 1)
     self.assertEqual(u2.followers.first().username, 'john')
 
@@ -39,5 +40,10 @@ class UserModelCase(unittest.TestCase):
     self.assertFalse(u1.is_following(u2))
     self.assertEqual(u1.followed.count(), 0)
     self.assertEqual(u2.followers.count(), 0)
+        
 
 
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
