8d71d7878be1ef64cd4a8fe7aa9152cc709e49bd
Connor Mackay <connor.mackay@gmail.com>
2023-01-09 14:32:43 -0500

    add and import babel

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask_login import LoginManager
 from config import Config
 from flask_mail import Mail
 from flask_moment import Moment
+from flask_babel import Babel
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,6 +18,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 moment = Moment(app)
+babel = Babel(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
